605eb790c43f0ce26ccb420d23af1d654773b8fa
ringo <ringo@Ringo.fritz.box>
2024-07-24 09:12:37 +0200

    issue #3 class MenuBar

---
+++ src/main/java/de/in/autoMower/sim/MethodsMenuBar.java
@@ -0,0 +1,27 @@
+package de.in.autoMower.sim;
+
+import java.awt.Image;
+
+
+public class MethodsMenuBar {
+
+	
+	protected void openProject() {
+	
+
+	}
+
+	protected void changeImage(Image image) {
+
+	}
+
+	protected void saveProject() {
+		
+	
+
+	}
+
+	protected void exit() {
+		System.exit(0);
+	}
+}
