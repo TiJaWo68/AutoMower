7e8e5ae660b927bca42b9dced360340064ed14be
Christopher Schmidt <christopher.schmidt@dedalus-group.com>
2024-07-26 09:53:28 +0200

    issue #5 add initial JUnit Test Class

---
+++ src/test/java/de/in/autoMower/sim/MathUtilTest.java
@@ -0,0 +1,56 @@
+/*
+*                C O P Y R I G H T  (c) 2022
+*                        DEDALUS SPA
+*                    All Rights Reserved
+*
+*
+*      THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
+*                        DEDALUS SPA
+*     The copyright notice above does not evidence any
+*    actual or intended publication of such source code.
+*/
+package de.in.autoMower.sim;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Christopher Schmidt <christopher.schmidt@dedalus.com>
+ */
+class MathUtilTest
+{
+    
+    @Test
+    void isEqualTrueTest() {
+        
+        float a = 4f;
+        float b = 4f;
+        
+        boolean result = MathUtil.isEqual( a, b );
+        
+        assertEquals(true, result);
+                
+    }
+    
+    @Test
+    void isEqualFalseTest() {
+        
+        float a = 4f;
+        float b = 5f;
+        
+        boolean result = MathUtil.isEqual( a, b );
+        
+        assertEquals(true, result);
+                
+    }
+    
+    
+    @Test
+    void test()
+    {
+        fail( "Not yet implemented" );
+    }
+    
+}
