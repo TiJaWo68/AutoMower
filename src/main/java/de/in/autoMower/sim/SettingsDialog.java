f8754e4f56662b2e49b833cf0df43689eba54a8c
t68 <till.woitendorf@gmail.com>
2025-12-29 20:12:48 +0100

    Update SettingsDialog to configure battery and energy parameters.

--- src/main/java/de/in/autoMower/sim/SettingsDialog.java
+++ src/main/java/de/in/autoMower/sim/SettingsDialog.java
@@ -12,26 +12,35 @@ import javax.swing.JOptionPane;
 
 public class SettingsDialog {
 
-	protected JFormattedTextField jTfSpeed, jTfWidth;
+	protected JFormattedTextField jTfSpeed, jTfWidth, jTfBattery, jTfConsumption, jTfCharge;
 
 	public SettingsDialog(AutoMowerModel autoMowerModel) {
 		JDialog dialog = new JDialog();
 		dialog.setTitle("Mower Specifications");
 		dialog.setLayout(new GridLayout(0, 2, 5, 5));
 
-		
 		NumberFormat numberformat = NumberFormat.getInstance(Locale.US);
-		numberformat.setMaximumFractionDigits(2);
+		numberformat.setMaximumFractionDigits(4);
 
-		JLabel jLTextWidth = new JLabel();
-		jLTextWidth.setText("   Width (cm)");
+		JLabel jLTextWidth = new JLabel("   Width (cm)");
 		jTfWidth = new JFormattedTextField(numberformat);
-		jTfWidth.setText(String.valueOf(numberformat.format(autoMowerModel.getMowingWidthInCm())));
+		jTfWidth.setText(numberformat.format(autoMowerModel.getMowingWidthInCm()));
 
-		JLabel jLTextSpeed = new JLabel();
-		jLTextSpeed.setText("   Speed (cm/sec)");
+		JLabel jLTextSpeed = new JLabel("   Speed (cm/sec)");
 		jTfSpeed = new JFormattedTextField(numberformat);
-		jTfSpeed.setText(String.valueOf(numberformat.format(autoMowerModel.getSpeedInCmPerSec())));
+		jTfSpeed.setText(numberformat.format(autoMowerModel.getSpeedInCmPerSec()));
+
+		JLabel jLTextBattery = new JLabel("   Battery Cap (Wh)");
+		jTfBattery = new JFormattedTextField(numberformat);
+		jTfBattery.setText(numberformat.format(autoMowerModel.batteryCapacityWh));
+
+		JLabel jLTextConsumption = new JLabel("   Cons. (Wh/cm)");
+		jTfConsumption = new JFormattedTextField(numberformat);
+		jTfConsumption.setText(numberformat.format(autoMowerModel.energyConsumptionWhPerCm));
+
+		JLabel jLTextCharge = new JLabel("   Charge (Wh/s)");
+		jTfCharge = new JFormattedTextField(numberformat);
+		jTfCharge.setText(numberformat.format(autoMowerModel.chargeRateWhPerSec));
 
 		JButton jBCancel = new JButton("Cancel");
 		jBCancel.addActionListener(e -> {
@@ -40,35 +49,42 @@ public class SettingsDialog {
 
 		JButton jbSave = new JButton("Save");
 		jbSave.addActionListener(e -> {
-
 			try {
-				if (jTfWidth.getText().isEmpty())
-					JOptionPane.showMessageDialog(null, "Please insert the width of mover");
-				else
-					autoMowerModel.setMowingWidthInCm(Double.parseDouble(jTfWidth.getText()));
+				autoMowerModel.setMowingWidthInCm(Double.parseDouble(jTfWidth.getText().replace(",", "")));
+				autoMowerModel.setSpeedInCmPerSec(Double.parseDouble(jTfSpeed.getText().replace(",", "")));
+				autoMowerModel.batteryCapacityWh = Double.parseDouble(jTfBattery.getText().replace(",", ""));
+				autoMowerModel.energyConsumptionWhPerCm = Double.parseDouble(jTfConsumption.getText().replace(",", ""));
+				autoMowerModel.chargeRateWhPerSec = Double.parseDouble(jTfCharge.getText().replace(",", ""));
 
-				if (jTfSpeed.getText().isEmpty())
-					JOptionPane.showMessageDialog(null, "Please insert the speed of mover");
-				else
-					autoMowerModel.setSpeedInCmPerSec(Double.parseDouble(jTfSpeed.getText()));
+				// Reset current battery if capacity changes significantly?
+				// For now just cap it.
+				if (autoMowerModel.currentBatteryWh > autoMowerModel.batteryCapacityWh) {
+					autoMowerModel.currentBatteryWh = autoMowerModel.batteryCapacityWh;
+				}
 
+				dialog.dispose();
 			} catch (NumberFormatException ex) {
-				System.err.println("Incorrect format");
-				JOptionPane.showMessageDialog(null, "Incorrect format");
+				JOptionPane.showMessageDialog(null, "Incorrect format: " + ex.getMessage());
 			}
 		});
-
-		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		dialog.setLocationRelativeTo(App.getApp());
-		dialog.setSize(300, 130);
-		dialog.setResizable(false);
 
 		dialog.add(jLTextWidth);
 		dialog.add(jTfWidth);
 		dialog.add(jLTextSpeed);
 		dialog.add(jTfSpeed);
+		dialog.add(jLTextBattery);
+		dialog.add(jTfBattery);
+		dialog.add(jLTextConsumption);
+		dialog.add(jTfConsumption);
+		dialog.add(jLTextCharge);
+		dialog.add(jTfCharge);
 		dialog.add(jBCancel);
 		dialog.add(jbSave);
+
+		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		dialog.setSize(350, 250);
+		dialog.setLocationRelativeTo(App.getApp());
+		dialog.setResizable(false);
 		dialog.setModal(true);
 		dialog.setVisible(true);
 	}
