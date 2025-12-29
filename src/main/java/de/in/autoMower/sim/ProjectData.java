d1ad35dfe7baa1219def2f3045ea8482b2c520bc
t68 <till.woitendorf@gmail.com>
2025-12-29 20:11:56 +0100

    Add ProjectData class for JSON serialization using Jackson.

---
+++ src/main/java/de/in/autoMower/sim/ProjectData.java
@@ -0,0 +1,93 @@
+package de.in.autoMower.sim;
+
+import java.awt.geom.Point2D;
+import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProjectData implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class PointDTO {
+        public double x;
+        public double y;
+
+        public PointDTO() {
+        }
+
+        public PointDTO(Point2D p) {
+            this.x = p.getX();
+            this.y = p.getY();
+        }
+
+        public Point2D toPoint() {
+            return new Point2D.Double(x, y);
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class MultiLineDTO {
+        public List<PointDTO> points;
+        public int colorRGB;
+        public boolean closed;
+
+        public MultiLineDTO() {
+        }
+
+        public MultiLineDTO(MultiLine2D ml) {
+            this.points = ml.points.stream().map(PointDTO::new).collect(Collectors.toList());
+            this.colorRGB = ml.color.getRGB();
+            this.closed = ml.closed;
+        }
+
+        public MultiLine2D toMultiLine() {
+            MultiLine2D ml = new MultiLine2D(new java.awt.Color(colorRGB));
+            if (points != null) {
+                for (PointDTO p : points) {
+                    ml.addPoint(p.toPoint());
+                }
+            }
+            if (closed)
+                ml.closePath();
+            return ml;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class MowerDTO {
+        public double speedInCmPerSec;
+        public double mowingWidthInCm;
+        public double batteryCapacityWh = 50.0;
+        public double energyConsumptionWhPerCm = 0.00045;
+        public double chargeRateWhPerSec = 0.02;
+        public PointDTO currentPosition;
+
+        public MowerDTO() {
+        }
+
+        public MowerDTO(AutoMowerModel mower) {
+            this.speedInCmPerSec = mower.getSpeedInCmPerSec();
+            this.mowingWidthInCm = mower.getMowingWidthInCm();
+            this.batteryCapacityWh = mower.batteryCapacityWh;
+            this.energyConsumptionWhPerCm = mower.energyConsumptionWhPerCm;
+            this.chargeRateWhPerSec = mower.chargeRateWhPerSec;
+            if (mower.getCurrentPosition() != null) {
+                this.currentPosition = new PointDTO(mower.getCurrentPosition());
+            }
+        }
+    }
+
+    public MultiLineDTO border;
+    public List<MultiLineDTO> obstacles;
+    public double calibration;
+    public MowerDTO mower;
+    public String backgroundImageBase64; // PNG image as Base64 string
+    public PointDTO chargingStation;
+
+    public ProjectData() {
+    }
+}
